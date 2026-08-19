48d49efa538f6cf9ff418e2a85b19a9379a88d89
tongyun09 <2012500360@qq.com>
2026-08-19 15:45:47 +0800

    重命名函数
    将函数名main修改为test

--- main.py
+++ main.py
@@ -7,11 +7,10 @@ def greet(name: str = "GitHub") -> str:
     """返回一句问候语。"""
     return f"Hello, {name}! 今天是 {date.today().isoformat()}"
 
-def main() -> None:
+def test() -> None:
     print(greet())
     print("这是我的第一个 GitHub 项目！")
     print("我的名字是tongyun,很高兴认识你！")
     
 if __name__ == "__main__":
-    main()
-    
+    test()
